049da9f5b264e73094c53abd16e4e2aae70d08da
Johnny-First <157603032+Johnny-First@users.noreply.github.com>
2024-02-02 14:28:21 +0300

    Deleting excess files

--- InputSystem.py
+++
@@ -1,9 +0,0 @@
-from pynput import keyboard
-from pynput import mouse
-
-class InputSystem:
-    def __init__(self, on_press, on_release):
-        keyboard_listener = keyboard.Listener( on_press=on_press, on_release=on_release)
-        keyboard_listener.start()
-
-
